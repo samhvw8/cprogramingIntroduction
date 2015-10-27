6d8380e3cf7178eb914bc8cd5a9cbfb3dfe4bc47
Sam Hoang <samhv.ict@gmail.com>
2015-10-27 21:40:15 +0700

    ...

--- test/1.c
+++ test/1.c
@@ -1,55 +1,70 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define INVALID -1
-#define maxoption 100
-#define tabs 8
-void mfflush();
+#define BOLDRED "\033[1;31m"
+#define BLUE "\x1b[34m"
+#define RESET "\x1b[0m"
+#define BLACK "\033[30m"
+#define BOLDBLUE "\033[1m\033[34m"
+#define BOLDBLACK "\033[1m\033[30m"
+#define BBLUE "\x1b[44m"
 
-typedef struct $player
-{
-    int win,lose;
-    
-} player;
-void reset(player *player1);
-void credit();
-int checkan(char arr[], int len);
-int str2numa(char arr[], int len);
-int lenstr(char arr[]);
-void pt(char *a);
-int GetMenu(char *a, int o);
-void repchar(int s, int e, char c); //print character  c with (s-e times) // print square like %-4d with varible ..
-int menuChoice(int numberOfOptions, ...);
-int main()
-{   
-    player p1;
-    int l, c, i = 0;
-    p1.win = 1;p1.lose = 0;
-    printf("%d   %d \n",p1.win,p1.lose );
-    reset(&p1);
-    printf("%d   %d \n",p1.win,p1.lose );
-    return 1;
-}
-void reset(player *player1){
-    player1->win= player1->lose = 0;
+void mfflush() {
+  int ch;
+  while((ch = getchar()) && ch != '\n' && ch != EOF);
 }
 
+ main ()
+ {
+  char horizontalframe[] = "________________________________________________________";
+  char tentruong[42];
+  char tenthe[32];
+  int id;
+  char lop[3];
+  char hoten[12];
+  int ngay, thang, nam;
+  char hokhau[18];
+  char nhomnganh[20];
+  int khoahoc;
+  int ngaygt, thanggt, namgt;
+  int ngayph, thangph, namph;
+  char mavach[] = "|||||||||||||||||||||||||||||||";
+  
+  printf("Nhap ten truong: \n");
+  scanf("%[^\n]",tentruong);
+  printf("Nhap ten the: \n");
+  scanf(" %[^\n]",tenthe);
+  printf("Nhap MSSV: \n");
+  scanf("%d",&id);
+  printf("Nhap ten lop: \n");
+  scanf("%s",lop);
+  printf("Nhap ho va ten: \n");
+  scanf(" %[^\n]",hoten);
+  printf("Nhap ngay sinh: \n");
+  scanf("%2d/%2d/%8d",&ngay,&thang,&nam);
+  printf("Nhap ho khau: \n");
+  scanf(" %[^\n]",hokhau);
+  printf("Nhap nhom nganh: \n");
+  scanf(" %[^\n]",nhomnganh);
+  printf("Nhap khoa hoc: \n");
+  scanf("%2d",&khoahoc);
+  printf("Nhap ngay gia tri: \n");
+  scanf("%2d/%2d/%4d",&ngaygt,&thanggt,&namgt);
+  printf("Nhap ngay phat hanh: \n");
+  scanf("%2d/%2d/%4d",&ngayph,&thangph,&namph);
 
-
-
-
-void credit()
-{
-    printf("\n################################################################################\n");
-    printf("################################################################################\n");
-    printf("#################  ####    ##  #######  #######  ##    ####  ###################\n");
-    printf("###################               ####  ####               #####################\n");
-    printf("######################               #  #               ########################\n");
-    printf("#######################           ####  ####           #########################\n");
-    printf("###########################      #####  #####      #############################\n");
-    printf("############################    ######  ######    ##############################\n");
-    printf("############################   #######  #######   ##############################\n");
-    printf("############################# ################## ###############################\n");
-    printf("################################################################################\n");
-    printf("################################################################################\n\n");
+  printf("%s\n",horizontalframe);
+ printf("%-1c"BOLDRED"%4s"RESET"%3s%48c\n",'|'," ___","___",'|');
+ printf("%-1c"BOLDRED"%4s"RESET"%3s"BOLDBLUE"%42s"RESET"%6c\n",'|'," | |","  |",tentruong,'|');
+ printf("%-1c"BOLDRED"%4s"RESET"%3s"BOLDRED"%32s"RESET"%16c\n",'|'," |_|","__|",tenthe,'|');
+ printf("|"BOLDRED"%.54s"RESET"|\n",horizontalframe);
+ printf("|%55c\n",'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" MSSV:"RESET" %-18d"RESET" "BLUE"Lop: "RESET"%3s %d%10c\n",'|','|',id,lop,2,'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" Ho ten: "RESET" %-12s"RESET"%24c\n",'|','|',hoten,'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" Ngay sinh: "RESET"%-.2d/%-.2d/%-8d"RESET""BLUE"%-3s"RESET"%17c\n",'|','|',ngay,thang,nam,"Nam",'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" Ho khau: "RESET"%18s"RESET"%18c\n",'|','|',hokhau,'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" Nhom nganh: "RESET"%20s"RESET"%13c\n",'|','|',nhomnganh,'|');
+ printf("%-1c"BBLUE"|%8c"RESET""BLUE" Khoa hoc: "RESET" K%2d"RESET" "BLUE" Co gia tri den: "RESET"%.2d/%.2d/%.4d"RESET"%3c\n",'|','|',59,ngaygt,thanggt,namgt,'|');
+ printf("|%52s  |\n",mavach);
+ printf("| NPH:%.2d/%.2d/%.4d%37s  |\n",ngayph,thangph,namph,mavach);
+ printf
+   ("|%.54s|\n",horizontalframe);
 }
--- week9/homework/cbv2.c
+++ week9/homework/cbv2.c
@@ -5,54 +5,68 @@
 #include <stdio.h>
 
 
-int main (){
-	int o,oo,input,f,base,lost; 
-	int z,i,j,choice,c;
+int main () {
+	int o, oo, input, f, base, lost;
+	int z, i, j, choice, c;
 
-for(;;){
-	printf("----------------------------\n1. Input positve Number on base 10\n2. Convert to binary\n3. Convert to Hex\n4. Convert to Octal\n5. Quit\n\tYour answer : ");scanf("%d",&choice);
-	switch(choice){
-		case 1: do{ printf("Input positve Number on base 10 : ");scanf("%d",&o); if(o<0)printf("Input positve number !!!\n");} while(o<0);break; 
-		case 2: printf("Binary : ");oo = o;base = 2;
-		while(oo > 0) {input = oo;i = 0;
-		    while(input > 0) {
-		        f = input%base;input=input/base;i++;
-		       }
-		       	if(c == 1) for(z = 1 ; z < (j - i);z++)printf("0");
-		       	printf("%d",f);lost = 1;
-		       	for (z = 1; z < i; ++z) lost =lost*base;
-		       	oo = oo - f*lost;
-		       	if (oo == 0) for (z = 1; z < i; ++z) printf("0");
-		       	j = i;
-		       	c = 1 ;
-		}printf("\n\n");break;
-		case 3: printf("Hex : ");oo = o;base = 16;
-		while(oo > 0) {input = oo;i = 0;
-		    while(input > 0) {
-		        f = input%base;input=input/base;i++;
-		       }
-		       	if(c == 1) for(z = 1 ; z < (j - i);z++)printf("0");
-		       	if(f < 10) printf("%d",f); else if (f == 10 )printf("A");
-		       	else if (f == 11 )printf("B");else if (f == 12 )printf("C");else if (f == 13 )printf("D");	
-		       	else if (f == 14 )printf("E");else if (f == 15 )printf("F");
-		       	lost = 1;
-		       	for (z = 1; z < i; ++z) lost =lost*base;
-		       	oo = oo - f*lost;
-		       	if (oo == 0) for (z = 1; z < i; ++z) printf("0");
-		       	j = i;
-		       	c = 1 ;
-		}printf("\n\n");break;
-		case 4: printf("Binary : ");oo = o;base = 8;
-		while(oo > 0) {input = oo;i = 0;
-		    while(input > 0) {
-		        f = input%base;input=input/base;i++;
-		       }
-		       	if(c == 1) for(z = 1 ; z < (j - i);z++)printf("0");
-		       	printf("%d",f);lost = 1;
-		       	for (z = 1; z < i; ++z) lost =lost*base;
-		       	oo = oo - f*lost;
-		       	if (oo == 0) for (z = 1; z < i; ++z) printf("0");
-		       	j = i;
-		       	c = 1 ;
-		}printf("\n\n");break;
-		case 5:return 0;	}}}
+	for (;;) {
+		printf("----------------------------\n1. Input positve Number on base 10\n2. Convert to binary\n3. Convert to Hex\n4. Convert to Octal\n5. Quit\n\tYour answer : "); scanf("%d", &choice);
+		switch (choice) {
+		case 1: do {
+				printf("Input positve Number on base 10 : ");
+				scanf("%d", &o); if (o < 0)printf("Input positve number !!!\n");
+			} while (o < 0);
+			break;
+		case 2: printf("Binary : ");
+			oo = o; base = 2;
+			while (oo > 0) {
+				input = oo; i = 0;
+				while (input > 0) {
+					f = input % base; input = input / base; i++;
+				}
+				if (c == 1) for (z = 1 ; z < (j - i); z++)printf("0");
+				printf("%d", f); lost = 1;
+				for (z = 1; z < i; ++z) lost = lost * base;
+				oo = oo - f * lost;
+				if (oo == 0) for (z = 1; z < i; ++z) printf("0");
+				j = i;
+				c = 1 ;
+			} printf("\n\n");
+			break;
+		case 3: printf("Hex : ");
+			oo = o; base = 16;
+			while (oo > 0) {
+				input = oo; i = 0;
+				while (input > 0) {
+					f = input % base; input = input / base; i++;
+				}
+				if (c == 1) for (z = 1 ; z < (j - i); z++)printf("0");
+				if (f < 10) printf("%d", f); else if (f == 10 )printf("A");
+				else if (f == 11 )printf("B"); else if (f == 12 )printf("C"); else if (f == 13 )printf("D");
+				else if (f == 14 )printf("E"); else if (f == 15 )printf("F");
+				lost = 1;
+				for (z = 1; z < i; ++z) lost = lost * base;
+				oo = oo - f * lost;
+				if (oo == 0) for (z = 1; z < i; ++z) printf("0");
+				j = i;
+				c = 1 ;
+			} printf("\n\n"); break;
+		case 4: printf("Binary : ");
+			oo = o; base = 8;
+			while (oo > 0) {
+				input = oo; i = 0;
+				while (input > 0) {
+					f = input % base; input = input / base; i++;
+				}
+				if (c == 1) for (z = 1 ; z < (j - i); z++)printf("0");
+				printf("%d", f); lost = 1;
+				for (z = 1; z < i; ++z) lost = lost * base;
+				oo = oo - f * lost;
+				if (oo == 0) for (z = 1; z < i; ++z) printf("0");
+				j = i;
+				c = 1 ;
+			} printf("\n\n"); break;
+		case 5: return 0;
+		}
+	}
+}
